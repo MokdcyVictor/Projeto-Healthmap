7bee9be32e6f67bb3795e7c6c7d2418dd432d34d
UBS I3B <ubsprojetoi3b@gmail.com>
2023-04-02 12:34:46 -0400

    commit

--- src/screen/View-Map/index.js
+++ src/screen/View-Map/index.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { View, Button,Text, BackHandler, TouchableOpacity, Image } from 'react-native';
+import firebase from '../../../firebase'
 import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
 import * as Location from 'expo-location';
 import styles from './style';
@@ -44,9 +45,10 @@ export default function ViewMap() {
         initialRegion={mapRegion}
         loadingEnabled
         showsUserLocation
+       
       >
         
-       <Marker coordinate={{
+       <Marker  pinColor='blue' coordinate={{
         latitude: -3.101582340298086,
         longitude:  -60.01413263166075,
         latitudeDelta: 0.0922,
@@ -54,7 +56,7 @@ export default function ViewMap() {
         onPress={()=>{
           navigation.navigate('Hospital', {id:"99aurx0psKEZu9Y3IP1z", img:'https://firebasestorage.googleapis.com/v0/b/projeto-healthmap.appspot.com/o/ubs%2Fhospital.jpg?alt=media&token=0f5e7154-0a46-4408-828c-9408ba0a9501'})
         }}></Marker> 
-        <Marker coordinate={{
+        <Marker pinColor='blue' coordinate={{
         latitude: -3.0767412,
         longitude: -59.9547822,
         latitudeDelta: 0.0922,
@@ -62,7 +64,7 @@ export default function ViewMap() {
         onPress={()=>{
           navigation.navigate('Hospital', {id:"7nIWYU2x3QvQYpCy9scK", img:'https://firebasestorage.googleapis.com/v0/b/projeto-healthmap.appspot.com/o/ubs%2Fanamariabraga.jpg?alt=media&token=0a803f7e-c810-4531-806f-9fb2079bb873'})
         }}></Marker>
-        <Marker coordinate={{
+        <Marker pinColor='blue' coordinate={{
         latitude: -3.1266332,
         longitude: -60.0130364,
         latitudeDelta: 0.0922,
@@ -70,7 +72,7 @@ export default function ViewMap() {
         onPress={()=>{
           navigation.navigate('Hospital', {id:"Pvd135QMqku2TZbJcZz6", img:'https://firebasestorage.googleapis.com/v0/b/projeto-healthmap.appspot.com/o/ubs%2FBalbinaMestrinho.jpg?alt=media&token=ba1b1203-51c2-4ed2-b1fa-8163eaa0f959'})
         }}></Marker>
-        <Marker coordinate={{
+        <Marker pinColor='blue' coordinate={{
         latitude: -3.0385201,
         longitude: -59.9435218,
         latitudeDelta: 0.0922,
@@ -78,7 +80,7 @@ export default function ViewMap() {
         onPress={()=>{
           navigation.navigate('Hospital', {id:"e1kCidi1o9P5aFeq8wMw", img:'https://firebasestorage.googleapis.com/v0/b/projeto-healthmap.appspot.com/o/ubs%2Fplat%C3%A3oaraujo.jpg?alt=media&token=8ad8ea11-61df-49f6-af8d-c64d5b83251d://firebasestorage.googleapis.com/v0/b/projeto-healthmap.appspot.com/o/ubs%2FBalbinaMestrinho.jpg?alt=media&token=ba1b1203-51c2-4ed2-b1fa-8163eaa0f959'})
         }}></Marker>
-        <Marker coordinate={{
+        <Marker pinColor='blue' coordinate={{
         latitude: -3.0296771,
         longitude: -59.9797242,
         latitudeDelta: 0.0922,
