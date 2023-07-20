6f9cfcfd80a6345935b03ed3954b53481c871d56
UBS I3B <ubsprojetoi3b@gmail.com>
2023-07-19 21:38:34 -0400

    commit

--- firebase.js
+++ firebase.js
@@ -4,12 +4,13 @@ import 'firebase/compat/firestore'
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBc7obDj5TlojqyiYQLmNMfsjiC6fEZZR8",
-  authDomain: "projeto-healthmap.firebaseapp.com",
-  projectId: "projeto-healthmap",
-  storageBucket: "projeto-healthmap.appspot.com",
-  messagingSenderId: "478984670151",
-  appId: "1:478984670151:web:42b99a71d622432a8967e2"
+  apiKey: "AIzaSyC_Th61sHOoOgU3D0B_LRl4G03jqRUaFJo",
+  authDomain: "healthmap-app.firebaseapp.com",
+  projectId: "healthmap-app",
+  storageBucket: "healthmap-app.appspot.com",
+  messagingSenderId: "621653444967",
+  appId: "1:621653444967:web:3feaf41ec66f3800b8c7c2",
+  measurementId: "G-MM53J0DVL2"
 };
 
 if(!firebase.apps.length){
